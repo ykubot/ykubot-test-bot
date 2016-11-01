478eba6bfe7d805da8138691b15d17c472afcaa2
ykubot <yuichikubota74@gmail.com>
2016-11-02 01:26:37 +0900

    fixed.

--- app.py
+++ app.py
@@ -67,11 +67,12 @@ def callback():
 
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8888, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+    # arg_parser = ArgumentParser(
+    #     usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    # )
+    # arg_parser.add_argument('-p', '--port', default=8888, help='port')
+    # arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    # options = arg_parser.parse_args()
+    #
+    # app.run(debug=options.debug, port=options.port)
+    app.run()
