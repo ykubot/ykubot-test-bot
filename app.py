14e92e14d9c597bba445f0851810a153da64a14a
ykubot <yuichikubota74@gmail.com>
2016-11-06 12:18:07 +0900

    edit debug.

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
     arg_parser.add_argument('-p', '--port', default=8888, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    arg_parser.add_argument('-d', '--debug', default=True, help='debug')
     options = arg_parser.parse_args()
 
     app.run(debug=options.debug, port=options.port)
