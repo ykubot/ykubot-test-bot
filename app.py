c90102b26117e0247746220b0842dcb4f1df008d
ykubot <yuichikubota74@gmail.com>
2016-11-06 12:23:44 +0900

    no message

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def callback():
     #     abort(400)
 
     # get request body as text
-    body = request.get_data(as_text=True)
+    body = request.get_json(as_text=True)
     app.logger.info("Request body: " + body)
 
     # parse webhook body
