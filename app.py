0c402fd7eda59f39a8520150dd4a98693c616d34
ykubot <yuichikubota74@gmail.com>
2016-11-06 11:50:14 +0900

    jsonify

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import sys
 from argparse import ArgumentParser
 
-from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort, render_template, jsonify
 from linebot import (
     LineBotApi, WebhookParser
 )
@@ -20,6 +20,7 @@ app = Flask(__name__)
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
@@ -40,6 +41,8 @@ def index():
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
+    # if not request.json:
+    #     abort(400)
 
     # get request body as text
     body = request.get_data(as_text=True)
@@ -65,7 +68,7 @@ def callback():
             event.reply_token,
             TextSendMessage(text)
         )
-    return 'OK'
+    return jsonify('OK')
 
 
 if __name__ == "__main__":
